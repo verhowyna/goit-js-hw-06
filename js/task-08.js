3826de16e0c3c8198725d0478f75ba0fef0dad08
verhowyna <verhowyna@ukr.net>
2023-08-27 22:14:53 +0300

    sunday-5

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
-
+const formElem = document.querySelector('.login-form');
+formElem.addEventListener('submit', onSubmit);
+function onSubmit(evt) {
+    evt.preventDefault();
+    const form = evt.currentTarget;
+    const userEmail = form.elements.email.value;
+    const userPassword = form.elements.password.value;
+        if (userEmail !== "" && userPassword !== "") {
+           const data = {
+            email: userEmail,
+            password: userPassword,
+        };
+            console.log(data);
+            form.reset();
+            return;
+    }
+     alert("Please, fill in all the fields");
+    
+    }
+    

633ae8d699ea8f008256e79d2def424182d7d757
verhowyna <verhowyna@ukr.net>
2023-08-28 12:52:05 +0300

    monday-1

--- js/task-09.js
+++ js/task-09.js
@@ -1,17 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, 0)}`
 }
 
-const btnChangeColor = document.querySelector('.change-color');
-const color = document.querySelector('.color');
-const body = document.querySelector('body');
-btnChangeColor.addEventListener("click", onClick);
-function onClick(evt) {
- const bodyColor = `#${Math.floor(Math.random() * 16777215)
-  .toString(16)
-  .padStart(6, 0)}`;
-  color.textContent = bodyColor;
-  body.style.backgroundColor = `${bodyColor}`
-}
+  const btnChangeColor = document.querySelector('.change-color');
+  const color = document.querySelector('.color');
+  const body = document.querySelector('body');
+  btnChangeColor.addEventListener("click", onClick);
+  function onClick(evt) {
+    const bodyColor = getRandomHexColor();
+    color.textContent = bodyColor;
+    body.style.backgroundColor = bodyColor;
+  }
